77c7d1ad453c9532a46227ceb8168091f29cce37
Kevin Liem <reeden88@gmail.com>
2015-10-09 16:53:10 +0700

    Add Game Component - Add Game Manager - Add Sound Manager - Bgm throughout the game

---
+++ Get The Shell/Assets/Scripts/GameComponent/BgmManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BgmManager : MonoBehaviour {
+
+    public AudioClip bgm;
+	
+    void Awake()
+    {
+        this.GetComponent<AudioSource>().clip = bgm;
+        this.GetComponent<AudioSource>().Play();
+    }
+}
